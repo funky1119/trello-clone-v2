178928731423c1e0f32cb9cb494aa677afac7bc6
“funky1119” <funky1119@naver.com>
2023-12-27 18:56:12 +0900

    recoil selecotr set 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-import GlobalStyle from "./styles/GlobalStyle";
+import { useRecoilState } from "recoil";
+import { hoursState, minutesState } from "./state/atorms";
+import { FormEvent } from "react";
 
 function App() {
+  const [minute, setMinute] = useRecoilState(minutesState);
+  const [hour, setHour] = useRecoilState(hoursState);
+
+  const onChageMinites = (event: FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setMinute(+value);
+  };
+
+  const onChageHours = (event: FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setHour(+value);
+  };
+
   return (
-    <>
-      <GlobalStyle />
-    </>
+    <div>
+      <input value={minute} onChange={onChageMinites} />
+      <input value={hour} onChange={onChageHours} />
+    </div>
   );
 }
 
--- src/state/atorms.ts
+++ src/state/atorms.ts
@@ -1,6 +1,18 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
-export const aa = atom({
-  key: "aa",
-  default: false,
+export const minutesState = atom({
+  key: "minutes",
+  default: 0,
+});
+
+export const hoursState = selector({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minutesState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minutesState, minutes);
+  },
 });
